67e051d515adc4872949cfe75e25eee4cb2b0f1c
scott <scott.a.benson189@gmail.com>
2021-01-30 14:33:47 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
 
 const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, '0.0.0.0', () => console.log(`Example app listening on port ${port}!`))
