1e033f925ee58da537c4fb47ce634ec990d35afd
scott <scott.a.benson189@gmail.com>
2021-01-30 21:55:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let DB_URI = `postgresql://`
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}drafts`
+  DB_URI = `${DB_URI}drafts_db`
 } else {
-  DB_URI = process.env.DB_URL || `${DB_URI}drafts`
+  DB_URI = process.env.DB_URL || `${DB_URI}drafts_db`
 }
 
 module.exports = DB_URI 
