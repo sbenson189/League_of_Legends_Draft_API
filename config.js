7e3a26c076dfb10ed0cde6b392df0ba5f9167eef
scott <scott.a.benson189@gmail.com>
2021-02-02 05:38:36 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 require("dotenv").config()
 
-// let DB_URI = `postgresql://`
-let DB_URI = 'postgres://allnftilsmwmlb:adaad000f1c44cbbc497391313af91b36ca54f3867484bdcc2fb93f7060217ca@ec2-54-146-91-153.compute-1.amazonaws.com:5432/d4ea02ile949ln'
+let DB_URI = `postgresql://`
 
 if (process.env.NODE_ENV === "test") {
   DB_URI = `${DB_URI}/drafts_db`
 } else {
-  // DB_URI = process.env.DB_URL || `${DB_URI}/drafts_db`
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}`
+  DB_URI = process.env.DATABASE_URL || `${DB_URI}/drafts_db`
 }
 
 module.exports = DB_URI 
